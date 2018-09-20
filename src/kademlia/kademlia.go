ff1b16d9a008c7c1ee9969707f79f63ba108fd53
MSI\darky <darkyll@orange.fr>
2018-09-20 19:03:44 +0200

    amélioration fonction listen kademlia + case ping

--- src/kademlia/kademlia.go
+++ src/kademlia/kademlia.go
@@ -1,7 +1,11 @@
 package kademlia
 
 import (
-    "strconv"
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"net"
+	"strconv"
 )
 
 type Kademlia struct {
@@ -32,12 +36,29 @@ func (kademlia *Kademlia) Listen(ip string, port int) {
         }
         
         inputBytes := make([]byte, 1024)
-        length, _ := c.Read(inputBytes)
+        length, _ := udpConn.Read(inputBytes)
         buf := bytes.NewBuffer(inputBytes[:length])
         
         decoder := gob.NewDecoder(buf)
         var header Header
         decoder.Decode(&header)
+
+        switch header.Type{
+
+		case MSG_PING :
+			//traiter le msgping
+			kademlia.RoutingTable.AddContact(NewContact(&(header.SrcID),IPToStr(header.SrcIP)))
+
+		case MSG_FIND_NODES :
+
+		case MSG_FIND_VALUE :
+
+		case MSG_STORE :
+
+
+			}
+
+
         
         // TODO: gérer le message dans un nouveau thread + AddContact
     }
