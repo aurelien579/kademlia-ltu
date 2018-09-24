57a4939134c6f717ba6c47dd11ebfe61065a7235
MSI\darky <darkyll@orange.fr>
2018-09-24 17:30:20 +0200

    amélioration fonction find nodes et find values reste a traiter gestion des contacts reçus et gestion du fichier reçu

--- src/kademlia/kademlia.go
+++ src/kademlia/kademlia.go
@@ -62,7 +62,7 @@ func (kademlia *Kademlia) Listen(ip string, port int) {
 
 		case MSG_FIND_VALUE:
 
-			kademlia.HandleFindValue(header)
+			kademlia.HandleFindValue(header, udpConn)
 
 		case MSG_STORE:
 
@@ -126,47 +126,7 @@ func (kademlia *Kademlia) HandleFindNodes(header Header, udpConn *net.UDPConn) {
 
 		var contacts []Contact = kademlia.RoutingTable.FindClosestContacts(&(findArguments.Key), int(findArguments.Count))
 
-		addr, _ := net.ResolveUDPAddr("udp", IPToStr(header.SrcIP)+":"+strconv.Itoa(int(header.SrcPort)))
-		conn, err := net.DialUDP("udp", nil, addr)
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		var buffer bytes.Buffer
-
-		enc := gob.NewEncoder(&buffer)
-		msg := Header{
-			SrcID:   *kademlia.Network.ID,
-			SrcIP:   kademlia.Network.IP,
-			SrcPort: kademlia.Network.Port,
-			Type:    MSG_RESPONSE,
-			SubType: MSG_FIND_NODES,
-		}
-
-		enc.Encode(msg)
-
-		time.Sleep(1 * time.Second)
-
-		conn.Write(buffer.Bytes())
-
-		buffer.Reset()
-
-		var contactsResults []ContactResult
-		for _, c := range contacts {
-			contactsResults = append(contactsResults, ContactResult{
-				ID:      c.ID.String(),
-				Address: c.Address,
-			})
-		}
-		fmt.Println("Sending: ", contactsResults)
-
-		enc.Encode(contactsResults)
-
-		time.Sleep(1 * time.Second)
-
-		conn.Write(buffer.Bytes())
+		kademlia.SendContact(header, contacts)
 
 		udpConn.Close()
 
@@ -180,6 +140,10 @@ func (kademlia *Kademlia) HandleFindNodes(header Header, udpConn *net.UDPConn) {
 		var contacts []ContactResult
 		err := decoder.Decode(&contacts)
 
+
+		//traiter les contacts
+
+
 		fmt.Println("Error: ", err)
 		fmt.Printf("Contacts received: %v\n", contacts)
 
@@ -188,7 +152,117 @@ func (kademlia *Kademlia) HandleFindNodes(header Header, udpConn *net.UDPConn) {
 
 }
 
-func (kademlia *Kademlia) HandleFindValue(header Header) {
+func (kademlia *Kademlia) HandleFindValue(header Header, udpConn *net.UDPConn) {
+
+	switch header.Type {
+
+	case MSG_REQUEST:
+		inputBytes := make([]byte, 1024)
+		length, _ := udpConn.Read(inputBytes)
+		buf := bytes.NewBuffer(inputBytes[:length])
+
+		decoder := gob.NewDecoder(buf)
+		var findArguments FindArguments
+		decoder.Decode(&findArguments)
+
+		fmt.Printf("Argument received: %v\n", findArguments)
+
+		if kademlia.Storage.Exists(KademliaID.String(findArguments.Key)) {
+
+			addr, _ := net.ResolveUDPAddr("udp", IPToStr(header.SrcIP)+":"+strconv.Itoa(int(header.SrcPort)))
+			conn, err := net.DialUDP("udp", nil, addr)
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			var buffer bytes.Buffer
+
+			enc := gob.NewEncoder(&buffer)
+			msg := Header{
+				SrcID:   *kademlia.Network.ID,
+				SrcIP:   kademlia.Network.IP,
+				SrcPort: kademlia.Network.Port,
+				Type:    MSG_RESPONSE,
+				SubType: MSG_FIND_VALUE,
+			}
+
+			enc.Encode(msg)
+
+			time.Sleep(1 * time.Second)
+
+			conn.Write(buffer.Bytes())
+
+			buffer.Reset()
+
+			conn.Write(kademlia.Storage.Read(KademliaID.String(findArguments.Key)))
+
+			udpConn.Close()
+
+
+
+		} else {
+
+			var contacts []Contact = kademlia.RoutingTable.FindClosestContacts(&(findArguments.Key), int(findArguments.Count))
+
+			kademlia.SendContact(header, contacts)
+
+			udpConn.Close()
+
+		}
+
+	case MSG_RESPONSE:
+
+		//JE RECOIS UN FICHIER
+
+	}
+
+}
+
+func (kademlia *Kademlia) SendContact(header Header, contacts []Contact) {
+
+	addr, _ := net.ResolveUDPAddr("udp", IPToStr(header.SrcIP)+":"+strconv.Itoa(int(header.SrcPort)))
+	conn, err := net.DialUDP("udp", nil, addr)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var buffer bytes.Buffer
+
+	enc := gob.NewEncoder(&buffer)
+	msg := Header{
+		SrcID:   *kademlia.Network.ID,
+		SrcIP:   kademlia.Network.IP,
+		SrcPort: kademlia.Network.Port,
+		Type:    MSG_RESPONSE,
+		SubType: MSG_FIND_NODES,
+	}
+
+	enc.Encode(msg)
+
+	time.Sleep(1 * time.Second)
+
+	conn.Write(buffer.Bytes())
+
+	buffer.Reset()
+
+	var contactsResults []ContactResult
+	for _, c := range contacts {
+		contactsResults = append(contactsResults, ContactResult{
+			ID:      c.ID.String(),
+			Address: c.Address,
+		})
+	}
+	fmt.Println("Sending: ", contactsResults)
+
+	enc.Encode(contactsResults)
+
+	time.Sleep(1 * time.Second)
+
+	conn.Write(buffer.Bytes())
 
 }
 
