738e608cc82e7b70f07dd3857c200bf1039daa09
Aurélien Martin <aurlienmartin@gmail.com>
2018-10-08 16:13:51 +0200

    Daemon protocol implemented

--- src/daemon/protocol.go
+++ src/daemon/protocol.go
@@ -1,7 +1,8 @@
 package daemon
 
 import (
-	"errors"
+	"bytes"
+	"encoding/gob"
 	"net"
 )
 
@@ -21,18 +22,71 @@ type Response struct {
 	result     string
 }
 
-func SendCommand(conn *net.UDPConn, command int, arg string) {
+func Encode(c *net.UDPConn, value interface{}) error {
+	var buffer bytes.Buffer
+	encoder := gob.NewEncoder(&buffer)
+	err := encoder.Encode(value)
 
+	if err != nil {
+		return err
+	}
+
+	c.Write(buffer.Bytes())
+
+	return nil
 }
 
-func ReadCommand(conn *net.UDPConn) (Command, error) {
-	return Command{}, errors.New("")
+func Decode(c *net.UDPConn, value interface{}) error {
+	inputBytes := make([]byte, 1024)
+
+	length, _, err := c.ReadFromUDP(inputBytes)
+	if err != nil {
+		return err
+	}
+
+	buf := bytes.NewBuffer(inputBytes[:length])
+	decoder := gob.NewDecoder(buf)
+	err = decoder.Decode(value)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func SendCommand(conn *net.UDPConn, command int, arg string) {
+	Encode(conn, Command{
+		command: command,
+		arg:     arg,
+	})
+}
+
+func ReadCommand(conn *net.UDPConn) (*Command, error) {
+	var command Command
+
+	err := Decode(conn, &command)
+	if err != nil {
+		return nil, err
+	}
+
+	return &command, nil
 }
 
 func SendResponse(conn *net.UDPConn, code int, result string) {
-
+	Encode(conn, Response{
+		resultCode: code,
+		result:     result,
+	})
 }
 
-func ReadResponse(conn *net.UDPConn) (Response, error) {
-	return Response{}, errors.New("")
+func ReadResponse(conn *net.UDPConn) (*Response, error) {
+	var response Response
+
+	err := Decode(conn, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response, nil
 }
